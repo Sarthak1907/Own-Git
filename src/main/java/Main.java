30926ffbf579069fe8722ffdc2a3c61615790bbc
Sarthak1907 <sarthaksrivastava230@outlook.com>
2024-06-09 19:22:30 +0530

    pass stage

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,14 +1,19 @@
 import java.io.BufferedReader;
+import java.io.ByteArrayInputStream;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.nio.file.Files;
+import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.HexFormat;
+import java.util.List;
 import java.util.zip.DeflaterOutputStream;
 import java.util.zip.InflaterInputStream;
 
@@ -67,28 +72,28 @@ public class Main {
         // Read File Contents in bytes
         byte[] bytes;
         try{
-          bytes=Files.readAllBytes(Paths.get(fileName));
+          bytes=Files.readAllBytes(Paths.get(fileName)); //Read all the content of the file in byte array
         }catch(IOException e){
           throw new RuntimeException(e);
         }
 
-        byte[] nullByte={0};
-        byte[] lengthBytes=String.valueOf(bytes.length).getBytes();
+        byte[] nullByte={0}; //Explained in line 83
+        byte[] lengthBytes=String.valueOf(bytes.length).getBytes(); //Representing the size of file in bytes
         MessageDigest md;
 
         try{
-          md=MessageDigest.getInstance("SHA-1");
-          md.update("blob ".getBytes());
-          md.update(lengthBytes);
-          md.update(nullByte);
-          md.update(bytes);
+          md=MessageDigest.getInstance("SHA-1"); //This line uses "SHA-1" hashing algorithm to create a 40 character hash code to identify a file uniquely
+          md.update("blob ".getBytes()); //This line updates the message digest with the bytes representing the string "blob ".
+          md.update(lengthBytes);//This line updates the message digest with the byte array containing the file size
+          md.update(nullByte);//This line updates the message digest with the single-byte array containing a 0 value so that when reading content from the file it could seperate content from its length. EX -> "11\0hello world"
+          md.update(bytes);//This line updates the message digest with the actual content of the file
         }catch(NoSuchAlgorithmException e){
           throw new RuntimeException(e);
         }
 
-        byte[] digest=md.digest();
+        byte[] digest=md.digest();//This line finalizes the hashing process and retrieves the SHA-1 hash of the combined data (including file information and content) as a byte array.
 
-        var hash=HexFormat.of().formatHex(digest);
+        var hash=HexFormat.of().formatHex(digest);//Converting the SHA-1 hash from a byte array into a human-readable string format.
 
         //The program will print out the 40-character SHA hash to output
         System.out.print(hash);
@@ -99,8 +104,9 @@ public class Main {
             String directoryHash = hash.substring(0, 2);
             String fileHash = hash.substring(2);
             File blobFile = new File("./.git/objects/" + directoryHash + "/" + fileHash);
-            blobFile.getParentFile().mkdirs();
+            blobFile.getParentFile().mkdirs();//It uses mkdirs() to create any missing parent directories for the blob file.
             blobFile.createNewFile();
+            
             //zlib compress the file
             FileOutputStream fos = new FileOutputStream(blobFile);
             DeflaterOutputStream dos = new DeflaterOutputStream(fos);
@@ -116,9 +122,68 @@ public class Main {
 
       }
 
+      case "ls-tree" -> {
+        String flag = null;
+           String treeSha;
+           if(args.length == 2){
+             //full
+             treeSha = args[1];
+           }else{
+             flag = args[1];
+             treeSha = args[2];
+           }
+             try {
+                 lsTree(treeSha, flag);
+             } catch (IOException e) {
+                 throw new RuntimeException(e);
+             }
+      }
+
       default -> System.out.println("Unknown command: " + command);
     }
 
-
   }
+
+  record TreeHeader(int contentSize){};
+  record TreeEntry(String mode, String name, byte[] sha1){};
+  private static void lsTree(String treeSha, String flag) throws IOException {
+    var objectPaths = shaToObjectPaths(treeSha);
+    byte[] bytes = Files.readAllBytes(objectPaths.fileName);
+    ByteArrayInputStream bais = new ByteArrayInputStream(bytes);
+    InflaterInputStream inflaterInputStream = new InflaterInputStream(bais);
+    var inflatedBytes = inflaterInputStream.readAllBytes();
+    boolean header = true;
+    StringBuilder acc = new StringBuilder();
+    List<TreeEntry> entries = new ArrayList<>();
+    for(int i=0;i<inflatedBytes.length;++i){
+      if(inflatedBytes[i] != '\0'){
+        acc.append((char)inflatedBytes[i]);
+      }else{
+        if(header){
+          //Print header
+          //System.out.printf("header: %s\n", acc.toString());
+          header = false;
+          acc = new StringBuilder();
+        }else{
+          //read additional 20 bytes for sha of entry
+          var parts = acc.toString().split(" ");
+          var mode = parts[0];
+          var name = parts[1];
+          var shaBytes = Arrays.copyOfRange(inflatedBytes, i, i+20);
+          entries.add(new TreeEntry(mode, name, shaBytes));
+          System.out.println(name);
+          i+=20;
+          acc = new StringBuilder();
+        }
+      }
+    }
+  }
+
+  record ObjectPaths(Path directory, Path fileName) {}
+       private static ObjectPaths shaToObjectPaths(String sha) {
+         String dir = sha.substring(0, 2);
+         String fileName = sha.substring(2);
+         return new ObjectPaths(Paths.get(".git", "objects", dir),
+                                Paths.get(".git", "objects", dir, fileName));
+       }
 }
