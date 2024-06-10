224234e29461ab68b497e514e9d79da7de97afe9
Sarthak1907 <sarthaksrivastava230@outlook.com>
2024-06-10 19:44:11 +0530

    pass stage

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,6 @@
 import java.io.BufferedReader;
 import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
@@ -12,8 +13,10 @@ import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Comparator;
 import java.util.HexFormat;
 import java.util.List;
+import java.util.Objects;
 import java.util.zip.DeflaterOutputStream;
 import java.util.zip.InflaterInputStream;
 
@@ -124,19 +127,31 @@ public class Main {
 
       case "ls-tree" -> {
         String flag = null;
-           String treeSha;
-           if(args.length == 2){
-             //full
-             treeSha = args[1];
-           }else{
-             flag = args[1];
-             treeSha = args[2];
-           }
-             try {
-                 lsTree(treeSha, flag);
-             } catch (IOException e) {
-                 throw new RuntimeException(e);
-             }
+        String treeSha;
+        if(args.length == 2){
+          //full
+          treeSha = args[1];
+        }else{
+          flag = args[1];
+          treeSha = args[2];
+        }
+        try {
+            lsTree(treeSha, flag);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+      }
+      case "write-tree" -> {
+        var workingDir = Paths.get("").toAbsolutePath().toString();
+        byte[] sha = null;
+        try {
+            sha = writeTree(Objects.requireNonNull(new File(workingDir).listFiles()));
+            System.out.println(toHexString(sha));
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        } catch (NoSuchAlgorithmException e) {
+            throw new RuntimeException(e);
+        }
       }
 
       default -> System.out.println("Unknown command: " + command);
@@ -180,10 +195,125 @@ public class Main {
   }
 
   record ObjectPaths(Path directory, Path fileName) {}
-       private static ObjectPaths shaToObjectPaths(String sha) {
-         String dir = sha.substring(0, 2);
-         String fileName = sha.substring(2);
-         return new ObjectPaths(Paths.get(".git", "objects", dir),
-                                Paths.get(".git", "objects", dir, fileName));
-       }
+  private static ObjectPaths shaToObjectPaths(String sha) {
+    String dir = sha.substring(0, 2);
+    String fileName = sha.substring(2);
+    return new ObjectPaths(Paths.get(".git", "objects", dir), Paths.get(".git", "objects", dir, fileName));
+  }
+
+  private static byte[] writeTree(File[] files) throws IOException, NoSuchAlgorithmException {
+    List<TreeEntry> entries = new ArrayList<>();
+    for (File f : files) {
+      if (f.isDirectory()) {
+        if (f.getName().equals(".git")) {
+          continue;
+        }
+        var subDirSha =
+            writeTree(Objects.requireNonNull(f.listFiles()));
+        // write subdir entry
+        entries.add(new TreeEntry("40000", f.getName(), subDirSha));
+      } else {
+        // write blob
+        var blobSha = hashObject("-w", f.getPath());
+        // write blob entry
+        entries.add(new TreeEntry("100644", f.getName(), blobSha));
+      }
+    }
+
+    //        entries.forEach(System.out::println);
+    //        System.out.println();
+    // sort by name
+    entries.sort(Comparator.comparing(k -> k.name));
+    ByteArrayOutputStream baos = new ByteArrayOutputStream();
+    for (var entry : entries) {
+      baos.write(entry.mode().getBytes());
+      baos.write(' ');
+      baos.write(entry.name.getBytes());
+      baos.write('\0');
+      baos.write(entry.sha1);
+    }
+    ByteArrayOutputStream treeStream = new ByteArrayOutputStream();
+    treeStream.write("tree ".getBytes());
+    treeStream.write(
+        String.valueOf(baos.toByteArray().length).getBytes());
+    treeStream.write('\0');
+    baos.writeTo(treeStream);
+
+    // get sha1 of bytes
+    MessageDigest md = MessageDigest.getInstance("SHA-1");
+    md.update(treeStream.toByteArray());
+    var digest = md.digest();
+    // from sha create dir and files in .git/objects
+    var objectPaths = shaToObjectPaths(toHexString(digest));
+    System.err.println("objctPaths: " + objectPaths);
+    Files.createDirectories(objectPaths.directory);
+    var blobFile = Files.createFile(objectPaths.fileName);
+    // write compressed content to file.
+    try (FileOutputStream fileOutputStream =
+             new FileOutputStream(blobFile.toString());
+         DeflaterOutputStream deflaterOutputStream =
+             new DeflaterOutputStream(fileOutputStream)) {
+      deflaterOutputStream.write(treeStream.toByteArray());
+      deflaterOutputStream.flush();
+      fileOutputStream.flush();
+    }
+
+    System.err.println("written file: " + blobFile.toString());
+    //        ByteArrayOutputStream compressedStream = new
+    //        ByteArrayOutputStream(); DeflaterOutputStream
+    //        deflaterOutputStream = new
+    //        DeflaterOutputStream(compressedStream);
+    //        treeStream.writeTo(deflaterOutputStream);
+    //        deflaterOutputStream.flush();
+    //        deflaterOutputStream.close();
+    //
+    //        try (FileOutputStream fileOutputStream = new
+    //        FileOutputStream(blobFile.toString())) {
+    //            fileOutputStream.write(compressedStream.toByteArray());
+    //            fileOutputStream.flush();
+    //        }
+    // return sha1
+    return digest;
+  }
+
+  private static byte[] hashObject(String flag, String filePath) throws IOException, NoSuchAlgorithmException {
+    assert flag.equals("-w");
+    var path = Path.of(filePath);
+    var bytes = Files.readAllBytes(path);
+    ByteArrayOutputStream byteArrayOutputStream =
+        new ByteArrayOutputStream();
+    byteArrayOutputStream.write("blob ".getBytes());
+    byteArrayOutputStream.write(
+        String.format("%d", bytes.length).getBytes());
+    byteArrayOutputStream.write((byte)'\0');
+    byteArrayOutputStream.write(bytes);
+    var blobBytes = byteArrayOutputStream.toByteArray();
+    ByteArrayOutputStream baos = new ByteArrayOutputStream();
+    DeflaterOutputStream dos = new DeflaterOutputStream(baos);
+    byteArrayOutputStream.writeTo(dos);
+    dos.flush();
+    dos.close();
+    // var deflatedBytes = baos.toByteArray();
+    MessageDigest md = MessageDigest.getInstance("SHA-1");
+    md.update(blobBytes);
+    var digest = md.digest();
+    var sha1String = toHexString(digest);
+    var dir = sha1String.substring(0, 2);
+    var blobFilename = sha1String.substring(2);
+    Files.createDirectories(Paths.get(".git", "objects", dir));
+    var blobFile = Files.createFile(Paths.get(".git", "objects", dir, blobFilename));
+    baos.writeTo(new FileOutputStream(blobFile.toString()));
+    //        System.out.println(sha1String);
+    return digest;
+  }
+
+  private static String toHexString(byte[] buf) {
+    StringBuilder stringBuilder = new StringBuilder();
+    for (var b : buf) {
+      stringBuilder.append(String.format("%02x", b));
+    }
+    return stringBuilder.toString();
+  }
+
+  
 }
